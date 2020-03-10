89fcbac0219117398a397728596399c50a099233
artha <aagret@petray.ch>
2020-03-10 12:49:33 +0100

    exclude payable securities (pending accounting) form fees

--- R/Source/getAccruedFees.R
+++ R/Source/getAccruedFees.R
@@ -2,7 +2,8 @@
 # extract Accrued Fees from NAV datas
 getAccruedFees <- function(db= nav) {
     
-    db <- db[Cat == "TRES" & !substr(Code, 1, 2) %in% c("DC", "BA"), ]
+    db <- db[Cat == "TRES",]
+    db <- db[!grep('DC|BA|CD100' , Code), ] #, 1, 2) %in% c("DC", "BA", "CD100"), ]
     db <- db[, .(Type= "AccruedFees",
                  Amount=sum(Amount)), by= .(Date, Port, Ccy)]
 } 
