0ff21f4aedf0648a06cf358a50572aa5ba632b7b
artha <aagret@petray.ch>
2020-12-01 14:40:40 +0100

    adjust cash calc

--- R/Source/getAllCash.R
+++ R/Source/getAllCash.R
@@ -22,41 +22,26 @@ getAllCash <- function(db= db1, ...){
     db[, (cum):= lapply(.SD, function(x) cumsum(x)),
        by= Ccy,
        .SDcols= cum]
-    
-    
-    # nonCum <- c("NetDividend", "PaidFees",
-    #             "PaidInterest", "SubRed")
-    # 
-    # db[, (nonCum):= lapply(.SD, function(x){ x[is.na(x)] <-0; x }), by= Ccy, .SDcols= nonCum]
-    # 
-    # cum <- colnames(db)[!colnames(db) %in% nonCum]
-    # db[, (cum):= lapply(.SD, function(x) {na.locf(x, na.rm=FALSE)}), by= Ccy, .SDcols= cum]
-    # 
-    # 
-    # db[is.na(db)] <- 0
-    # 
-    db[, CashInNav:=  Cash + AccruedFees + PendingDividend, by= Ccy]
-    db[, TotTax:=     PendingWh_Tax + Wh_Tax, by= Ccy]
+
+    db[, CashInNav:=  Cash + AccruedFees + Margin, by= Ccy]
+    db[, TotTax:=     Wh_Tax, by= Ccy]
     db[, TotFees:=    AccruedFees + PaidFees, by= Ccy]
     db[, OffsetCash:= CashInNav - TotTax - TotFees,   by= Ccy]
-    # 
-    # # calc all taxes
-    # db[, Tax:= na.locf( PendingWh_Tax), by= .(Ccy)]
-    # db[, Tax:= diff(c(0, PendingWh_Tax)), by= .(Ccy)]
-    # db[, Tax:= min(0, Tax), by= .(Date, Ccy)]
-    # db[, Tax:= cumsum(Tax), by= Ccy]
+
+    # carry over previous values
+    dts <- unique(db$Date)
     
-    # # calc all Fees
-    # db[, Fees:= diff(c(0, AccruedFees)), by= Ccy]
-    # db[, Fees:= min(0, Fees), by= .(Ccy, Date)]
-    # db[, Fees:= cumsum(Fees), by= Ccy]
-    # 
-    # db[, Fees2:= Fees+PaidFees, by=Ccy]
-    # db[, Fees3:= Fees+ shift(PaidFees, 2), by=Ccy]
-    # 
-    # # calc offset cash
-    # db[, OffsetCash:= Cash + Margin + PendingDividend - Tax - Fees + AccruedFees + PendingDividend , by= Ccy]
-    
+    for (dt in 2:length(dts)) {
+        
+        now  <- db[Date==dts[dt]]
+        prev <- db[Date==dts[dt-1]]
+        
+        missing <- prev[!Ccy %in% now$Ccy]
+        missing$Date <- dts[dt]
+        
+        db <- rbind(db, missing)
+        setkey(db, Date)
+    }
     
     return(db)
     
